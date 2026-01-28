dd6a7a1b735f9689baaa4d6b9ffa9492159b694f
TheWalkingForest <fwalker@tutamail.com>
2026-01-28 11:17:14 -0800

    Remove dhcpcd service

--- shepherd.scm
+++ shepherd.scm
@@ -2,8 +2,6 @@
              (shepherd service repl)
              (ice-9 ftw))
 
-;; Auto-load all service definitions from the services directory
-;; Each service file should call (register-services (list service-name)) itself
 (let ((services-dir  "/usr/etc/shepherd.d"))
   (for-each
     (lambda (file)
@@ -13,20 +11,11 @@
                  (lambda (f) (string-suffix? ".scm" f)))
         '())))
 
-(define dhcpcd
-  (service
-    '(dhcpcd)
-    #:start (make-forkexec-constructor
-              '("dhcpcd" "-B" "-M"))
-    #:stop (make-kill-destructor)
-    #:respawn? #t))
-
-(register-services (list (repl-service) dhcpcd))
+(register-services (list (repl-service)))
 (start-in-the-background
   '(agetty-tty1
     agetty-tty2
     agetty-tty3
     agetty-tty4
     agetty-tty5
-    agetty-tty6
-    dhcpcd))
+    agetty-tty6))
